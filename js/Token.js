480c77639b998cd453dd9609785b3cca9373b733
Dulio Denis <dulio.denis@yahoo.com>
2020-10-18 12:57:56 -0400

    Render the token into the DOM

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,23 @@ class Token {
     this.id = `token-${index}-${owner.id}`
     this.dropped = false
   }
+
+  /** 
+   * Renders the tokens into the DOM
+   */
+  drawHTMLToken() {
+    const token = document.createElement('div')
+    document.getElementById('game-board-underlay').appendChild(token)
+    token.setAttribute('id', this.id)
+    token.setAttribute('class', 'token')
+    token.style.backgroundColor = this.owner.color
+    return token
+  }
+
+  /** 
+   * Token getter method
+   */
+  get htmlToken() {
+    return this.drawHTMLToken
+  }
 }
